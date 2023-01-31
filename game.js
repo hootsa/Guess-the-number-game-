2401d3402739442dc158b5346e673d71934ab6f0
Hootsa <hootsa.kh@gmail.com>
2023-01-31 13:24:38 -0800

    add again btn in js and disabled btn

--- game.js
+++ game.js
@@ -2,23 +2,36 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let value = getRandomInt(0, 20);
+let value = getRandomInt(1, 20);
 console.log(value);
 const guessResultElement = document.getElementById("guessResult");
 const resultStatmentElement = document.getElementById("resultStatment");
+const checkInputElement = document.getElementById("numberSearch");
 
 function checkInputNum() {
-  const checkInputElement = document.getElementById("numberSearch");
   const inputValue = checkInputElement.value;
-  const converNum = Number(inputValue);
+  const convertNum = Number(inputValue);
 
   guessResultElement.innerHTML = value;
-  if (value === converNum) {
+
+  if (value === convertNum) {
     resultStatmentElement.innerHTML = " 🎉 Correct Number!";
   } else {
     resultStatmentElement.innerHTML = " ❌ Wrong Number!";
   }
+  checkInputElement.disabled = true;
 }
 
 const checkBtnElement = document.getElementById("checkBtn");
 checkBtnElement.addEventListener("click", checkInputNum);
+
+const againBtnElement = document.getElementById("againButton");
+againBtnElement.addEventListener("click", againBtnWork);
+
+function againBtnWork() {
+  checkInputElement.disabled = false;
+  value = getRandomInt(1, 20);
+  guessResultElement.innerHTML = "?";
+  resultStatmentElement.innerHTML = "";
+  checkInputElement.value = "";
+}
