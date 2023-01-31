e7e637a26b025149b7fa78d62a696715622542b9
Hootsa <hootsa.kh@gmail.com>
2023-01-30 18:05:29 -0800

    write js logics

--- game.js
+++ game.js
@@ -1,1 +1,28 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+let value = getRandomInt(0, 20);
+
+const checkInputElement = document.getElementById("numberSearch");
+const inputValue = checkInputElement.value;
+const converNum = Number(inputValue);
+
+const guessResultElement = document.getElementById("guessResult");
+const resultStatmentElement = document.getElementById("resultStatment");
+
+function checkInputNum() {
+  guessResultElement.innerHTML = value;
+  if (value === inputValue) {
+    resultStatmentElement.innerHTML = " 🎉 Correct Number!";
+  } else {
+    resultStatmentElement.innerHTML = " ❌ Wrong Number!";
+  }
+  return resultStatmentElement;
+}
+
+const checkBtnElement = document.getElementById("checkBtn");
+checkBtnElement.addEventListener("click", () => {
+  const newValue = checkInputNum();
+  return newValue;
+});
