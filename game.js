fed05a6474abd53fb67137c357b74c95006e58f2
Hootsa <hootsa.kh@gmail.com>
2023-01-31 17:40:16 -0800

    make score function in js

--- game.js
+++ game.js
@@ -7,19 +7,37 @@ console.log(value);
 const guessResultElement = document.getElementById("guessResult");
 const resultStatmentElement = document.getElementById("resultStatment");
 const checkInputElement = document.getElementById("numberSearch");
+const scoreElement = document.getElementById("score");
 
+let maxScore = 20;
+scoreElement.innerHTML = "💯 score:" + maxScore;
 function checkInputNum() {
+  let minScore = 0;
+
   const inputValue = checkInputElement.value;
+  if (inputValue === "") {
+    return;
+  }
   const convertNum = Number(inputValue);
 
-  guessResultElement.innerHTML = value;
-
-  if (value === convertNum) {
-    resultStatmentElement.innerHTML = " 🎉 Correct Number!";
+  if (convertNum > value && maxScore !== minScore) {
+    resultStatmentElement.innerHTML = "Your Number is too BIG!";
+    maxScore -= 1;
+    scoreElement.innerHTML = "💯 score:" + maxScore;
+  } else if (convertNum < value && maxScore !== minScore) {
+    resultStatmentElement.innerHTML = " Your Number is too SMALL!";
+    maxScore -= 1;
+    scoreElement.innerHTML = "💯 score:" + maxScore;
   } else {
-    resultStatmentElement.innerHTML = " ❌ Wrong Number!";
+    resultStatmentElement.innerHTML = " 🎉 Correct Number!";
+    scoreElement.innerHTML = "💯 score:" + maxScore;
+    guessResultElement.innerHTML = value;
   }
-  checkInputElement.disabled = true;
+  if (maxScore === minScore) {
+    scoreElement.innerHTML = "💯 score:" + minScore;
+  }
+
+  checkInputElement.innerHTML = "";
 }
 
 const checkBtnElement = document.getElementById("checkBtn");
@@ -31,6 +49,7 @@ againBtnElement.addEventListener("click", againBtnWork);
 function againBtnWork() {
   checkInputElement.disabled = false;
   value = getRandomInt(1, 20);
+  console.log(value);
   guessResultElement.innerHTML = "?";
   resultStatmentElement.innerHTML = "";
   checkInputElement.value = "";
